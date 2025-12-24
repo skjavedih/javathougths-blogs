93c9967514c0ea7f276bac34ceb98d081d095972
Javed Shaikh <javedshaikh@Javeds-Mac-mini.local>
2025-12-24 16:51:25 +0530

    Initial changes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  author: 'Javed Shaikh',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
