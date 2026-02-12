7ec62996380fde5ccb6829773b1d26361ffc44e0
Javed Shaikh <javedshaikh@Javeds-Mac-mini.local>
2026-02-12 06:40:34 +0530

    Corrected menu items

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,15 +26,15 @@ const Header = () => {
           )}
         </div>
       </Link>
-      <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
+      <div className="flex items-center space-x-4 leading-5 md:-mr-6 md:space-x-6">
+        <div className="no-scrollbar hidden items-center gap-x-1 md:flex lg:gap-x-3">
           {headerNavLinks
             .filter((link) => link.href !== '/')
             .map((link) => (
               <Link
                 key={link.title}
                 href={link.href}
-                className="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100"
+                className="hover:text-primary-500 dark:hover:text-primary-400 px-1.5 py-1 text-sm font-medium whitespace-nowrap text-gray-900 lg:px-2 lg:text-base dark:text-gray-100"
               >
                 {link.title}
               </Link>
