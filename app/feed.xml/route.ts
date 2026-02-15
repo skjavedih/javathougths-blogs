219e569d965bce1531020f1ac6edf027688724a3
Javed Shaikh <javedshaikh@Javeds-Mac-mini.local>
2026-02-15 09:17:05 +0530

    Fixed conflicing public file error for RSS feed

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -10,32 +10,32 @@ import { headers } from 'next/headers'
 export const dynamic = 'force-static'
 
 const generateRssItem = (config: any, post: any) => `
-  < item >
-  <guid>${config.siteUrl} /blog/${post.slug} </guid>
-    < title > ${escape(post.title)} </title>
-      < link > ${config.siteUrl} /blog/${post.slug} </link>
+  <item>
+    <guid>${config.siteUrl}/blog/${post.slug}</guid>
+    <title>${escape(post.title)}</title>
+    <link>${config.siteUrl}/blog/${post.slug}</link>
     ${post.summary && `<description>${escape(post.summary)}</description>`}
-<pubDate>${new Date(post.date).toUTCString()} </pubDate>
-  < author > ${config.email} (${config.author})</author>
+    <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+    <author>${config.email} (${config.author})</author>
     ${post.tags && post.tags.map((t: string) => `<category>${t}</category>`).join('')}
-</item>
-  `
+  </item>
+`
 
 const generateRss = (config: any, posts: any, page = 'feed.xml') => `
-  < rss version = "2.0" xmlns: atom = "http://www.w3.org/2005/Atom" >
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
-    <title>${escape(config.title)} </title>
-      < link > ${config.siteUrl} /blog</link >
-        <description>${escape(config.description)} </description>
-          < language > ${config.language} </language>
-            < managingEditor > ${config.email} (${config.author})</managingEditor>
-              < webMaster > ${config.email} (${config.author})</webMaster>
-                < lastBuildDate > ${new Date(posts[0].date).toUTCString()} </lastBuildDate>
-                  < atom:link href = "${config.siteUrl}/${page}" rel = "self" type = "application/rss+xml" />
-                    ${posts.map((post: any) => generateRssItem(config, post)).join('')}
-</channel>
+      <title>${escape(config.title)}</title>
+      <link>${config.siteUrl}/blog</link>
+      <description>${escape(config.description)}</description>
+      <language>${config.language}</language>
+      <managingEditor>${config.email} (${config.author})</managingEditor>
+      <webMaster>${config.email} (${config.author})</webMaster>
+      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+      <atom:link href="${config.siteUrl}/${page}" rel="self" type="application/rss+xml"/>
+      ${posts.map((post: any) => generateRssItem(config, post)).join('')}
+    </channel>
   </rss>
-    `
+`
 
 export async function GET() {
   const publishPosts = allBlogs.filter((post) => post.draft !== true)
